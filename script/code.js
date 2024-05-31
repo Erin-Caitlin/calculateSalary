156d3533e4beb8c21aa2e8adc2201b27e072219a
Erin-Caitlin <erinwilliamscat@gmail.com>
2024-05-31 15:10:47 +0200

    function

---
+++ script/code.js
@@ -0,0 +1,20 @@
+let firstName = document.querySelector('#firstName')
+let lastName = document.querySelector('#lastname')
+let age = document.querySelector('#age')
+let dateOfBirth = document.querySelector('#Date-of-birth')
+
+let gender = document.querySelector('#gender')
+let title = document.querySelector('#title')
+let languages = document.querySelector('#languages')
+let employmentStatus = document.querySelector('#work-status')
+
+let workHours = document.querySelector('#working-hours')
+let rate = document.querySelector('#rate')
+let Btn = document.querySelector('[calculate]')
+
+Btn.addEventListener('click',)
+function completedForm() {
+    if (firstName.value == "") {
+        alert("Please answer all questions");
+    }
+}
